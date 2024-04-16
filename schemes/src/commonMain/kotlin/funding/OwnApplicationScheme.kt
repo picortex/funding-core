8abe65a8bf5705d4fa48300b98166c727cafad60
andylamax <andylamax@programmer.net>
2024-04-16 06:39:59 +0300

    A monitor can now retrieve the list of their applications

--- schemes/src/commonMain/kotlin/funding/OwnApplicationScheme.kt
+++ schemes/src/commonMain/kotlin/funding/OwnApplicationScheme.kt
@@ -3,9 +3,12 @@
 package funding
 
 import funding.params.ApplicationParams
+import kollections.List
 import koncurrent.Later
 import kotlinx.JsExport
+import kronecker.LoadOptions
 
 interface OwnApplicationScheme : ApplicationScheme {
     fun create(params: ApplicationParams): Later<Application>
+    fun list(options: LoadOptions = LoadOptions()): Later<List<Application>>
 }
